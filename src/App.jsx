4a046187b68e1128efa136b2a055d34746849a26
David Pam <niratpam@gmail.com>
2022-12-09 01:15:34 +0100

    Setup Die Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import Die from './Die'
 import './App.css'
 
 function App() {
+  let dataArr = [
+    {value: 1, id: 1},
+    {value: 2, id: 2},
+    {value: 4, id: 3},
+    {value: 6, id: 4},
+    {value: 3, id: 5},
+    {value: 5, id: 6},
+    {value: 1, id: 7},
+    {value: 6, id: 8},
+    {value: 5, id: 9},
+    {value: 1, id: 10}
+  ]
+  let tenDices = dataArr.map(data => (
+    <Die value={data.value} key={data.id} />
+  ));
   return (
-    <main></main>
+    <main>
+      <div className="dice-container">
+        {tenDices}
+      </div>
+    </main>
   )
 }
 
